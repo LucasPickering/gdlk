bffcc5ed8eb87b5cd2dbe883ac3e82afe8a72ad0
John Murray <5672686+JRMurr@users.noreply.github.com>
2019-11-24 15:40:02 -0500

    Setup paring

--- lib/src/compiler/parse.rs
+++ lib/src/compiler/parse.rs
@@ -1,5 +1,106 @@
-use crate::{ast::Program, compiler::Compiler, error::CompileError};
+use crate::{
+    ast::{Instr, LangValue, Program, StackIdentifier},
+    compiler::Compiler,
+    error::CompileError,
+};
+use nom::{
+    branch::alt,
+    bytes::complete::tag_no_case,
+    character::complete::{char, digit1, multispace0},
+    combinator::map_res,
+    multi::{many0, many1},
+    sequence::{delimited, preceded, terminated},
+    IResult,
+};
 use std::io::Read;
+
+fn parse_read(input: &str) -> IResult<&str, Instr> {
+    // tag_no_case returns a (str, str) tuple
+    // first element is whats left to parse
+    // second is what matched
+    let (input, _) = tag_no_case("Read")(input)?;
+    Ok((input, Instr::Read))
+}
+
+fn parse_write(input: &str) -> IResult<&str, Instr> {
+    let (input, _) = tag_no_case("Write")(input)?;
+    Ok((input, Instr::Write))
+}
+
+fn parse_set(input: &str) -> IResult<&str, Instr> {
+    let (input, _) = tag_no_case("Set")(input)?;
+    let (input, val) = preceded(
+        multispace0,
+        map_res(digit1, |s: &str| s.parse::<LangValue>()),
+    )(input)?;
+    Ok((input, Instr::Set(val)))
+}
+
+fn parse_push(input: &str) -> IResult<&str, Instr> {
+    let (input, _) = tag_no_case("Push")(input)?;
+    let (input, val) = preceded(
+        multispace0,
+        map_res(digit1, |s: &str| s.parse::<StackIdentifier>()),
+    )(input)?;
+    Ok((input, Instr::Push(val)))
+}
+
+fn parse_pop(input: &str) -> IResult<&str, Instr> {
+    let (input, _) = tag_no_case("Pop")(input)?;
+    let (input, val) = preceded(
+        multispace0,
+        map_res(digit1, |s: &str| s.parse::<StackIdentifier>()),
+    )(input)?;
+    Ok((input, Instr::Pop(val)))
+}
+
+fn parse_if(input: &str) -> IResult<&str, Instr> {
+    let (input, _) = tag_no_case("If")(input)?;
+    let (input, res) = parse_body(input)?;
+    Ok((input, Instr::If(res)))
+}
+
+fn parse_while(input: &str) -> IResult<&str, Instr> {
+    let (input, _) = tag_no_case("While")(input)?;
+    let (input, res) = parse_body(input)?;
+    Ok((input, Instr::While(res)))
+}
+
+fn try_each(input: &str) -> IResult<&str, Instr> {
+    let (input, res) = preceded(
+        multispace0,
+        alt((
+            parse_read,
+            parse_write,
+            parse_set,
+            parse_push,
+            parse_pop,
+            parse_if,
+            parse_while,
+        )),
+    )(input)?;
+    Ok((input, res))
+}
+
+// Parse the body of an if or while statement
+//
+// something like (\s*{<BODY>\s*})
+fn parse_body(input: &str) -> IResult<&str, Vec<Instr>> {
+    // multispace0 matches 0 or more whitespace chars (including new lines)
+    let (input, res) = delimited(
+        preceded(multispace0, char('{')),
+        many0(try_each), // many0 will match 0 more, so the body could be empty
+        preceded(multispace0, char('}')),
+    )(input)?;
+    Ok((input, res))
+}
+
+fn parse_gdlk(input: &str) -> IResult<&str, Vec<Instr>> {
+    // parses the whole program followed by 0 or more whitespace chars
+    // using many1 so the program needs at least one instr
+    let (input, res) = terminated(many1(try_each), multispace0)(input)?;
+    Ok((input, res))
+}
 
 impl Compiler<()> {
     /// Parses source code from the given input, into an abstract syntax tree.
@@ -11,9 +112,122 @@ impl Compiler<()> {
         source
             .read_to_string(&mut source_buffer)
             .map_err(|err| CompileError::ParseError(format!("{}", err)))?;
-        // TODO real parsing here
-        let program = serde_json::from_str(&source_buffer)
-            .map_err(|err| CompileError::ParseError(format!("{}", err)))?;
-        Ok(Compiler(program))
+        match parse_gdlk(&source_buffer) {
+            // TODO: can probably refactor the parser funcs to use
+            // Verbose error to make the errors nicer
+            // example: https://github.com/Geal/nom/blob/master/examples/s_expression.rs
+            Ok((_, body)) => {
+                let prog = Program { body };
+                Ok(Compiler(prog))
+            }
+            Err(nom::Err::Error((_input, e))) => {
+                Err(CompileError::ParseError(e.description().to_string()))
+            }
+            Err(nom::Err::Incomplete(_needed)) => {
+                // TODO: better ass
+                Err(CompileError::ParseError("ass".to_string()))
+            }
+            Err(nom::Err::Failure((_input, e))) => {
+                Err(CompileError::ParseError(e.description().to_string()))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_read_write() {
+        assert_eq!(
+            parse_gdlk(
+                "
+            ReAd
+            WrItE
+        "
+            ),
+            Ok(("", vec![Instr::Read, Instr::Write]))
+        )
+    }
+    #[test]
+    fn test_set() {
+        assert_eq!(parse_gdlk("Set 4"), Ok(("", vec![Instr::Set(4),])))
+    }
+
+    #[test]
+    fn test_push() {
+        assert_eq!(parse_gdlk("Push 4"), Ok(("", vec![Instr::Push(4),])))
+    }
+
+    #[test]
+    fn test_pop() {
+        assert_eq!(parse_gdlk("Pop 4"), Ok(("", vec![Instr::Pop(4),])))
+    }
+
+    #[test]
+    fn test_parse_if() {
+        assert_eq!(
+            parse_gdlk(
+                "IF {
+            Read
+            write
+        }"
+            ),
+            Ok(("", vec![Instr::If(vec![Instr::Read, Instr::Write,])]))
+        )
+    }
+
+    #[test]
+    fn test_parse_while() {
+        assert_eq!(
+            parse_gdlk(
+                "WHiLE {
+            READ
+            Write
+        }"
+            ),
+            Ok(("", vec![Instr::While(vec![Instr::Read, Instr::Write,])]))
+        )
+    }
+
+    #[test]
+    fn test_parse_empty_if_and_while() {
+        assert_eq!(
+            parse_gdlk("while {}if{}"),
+            Ok(("", vec![Instr::While(vec![]), Instr::If(vec![])]))
+        )
+    }
+
+    #[test]
+    fn test_parse_simple_file() {
+        assert_eq!(
+            parse_gdlk(
+                "
+            Read
+            Set 2
+            Write
+            Read
+            Set 3
+            Write
+            Read
+            Set 4
+            Write
+        "
+            ),
+            Ok((
+                "",
+                vec![
+                    Instr::Read,
+                    Instr::Set(2),
+                    Instr::Write,
+                    Instr::Read,
+                    Instr::Set(3),
+                    Instr::Write,
+                    Instr::Read,
+                    Instr::Set(4),
+                    Instr::Write
+                ]
+            ))
+        )
     }
 }
