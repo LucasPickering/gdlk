ff9412d76e923435efed6ef28bb32daa71f9ecf3
Lucas Pickering <lucas@lucaspickering.me>
2019-12-08 12:31:22 -0500

    Rename max_stack_size->max_stack_length, make it a required field

--- api/src/schema.rs
+++ api/src/schema.rs
@@ -3,7 +3,7 @@ table! {
         id -> Int4,
         num_registers -> Int4,
         num_stacks -> Int4,
-        max_stack_size -> Nullable<Int4>,
+        max_stack_length -> Int4,
         input -> Array<Int4>,
         expected_output -> Array<Int4>,
     }
