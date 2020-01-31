f64ed287fac6df04929974bd5cf626190d35e710
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-01-30 23:17:29 -0500

    start trying to use WasmTerminal

---
+++ frontend/src/terminal/Terminal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+// import WasmTerminal from "@wasmer/wasm-terminal/lib/optimized/wasm-terminal.esm";
+export class Terminal extends React.Component {
+  render() {
+    return <h1>Hello poop</h1>;
+  }
+}
