e95b04cb1388dd573b1defa25c2e8a054ee4f8bd
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-02-01 04:10:05 -0500

    setup eslint and prettier

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import Terminal from "./Terminal";
+import React from 'react';
+import './App.css';
+import Terminal from './Terminal';
 const App: React.FC = () => {
   return (
     <div className="App">
