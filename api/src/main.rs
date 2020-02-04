9ebec46553dc3b87bb2b9bccce85c25022d47be7
Lucas Pickering <lucas@lucaspickering.me>
2020-02-03 21:37:49 -0500

    Upgrade nightly, disable a lint that is now failing

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,6 @@
 #![deny(clippy::all, unused_must_use, unused_imports)]
+// Need to allow this because Diesel's macros violate it
+#![allow(clippy::single_component_path_imports)]
 #![feature(const_fn, trait_alias)]
 
 // Diesel hasn't fully moved to Rust 2018 yet so we need this
