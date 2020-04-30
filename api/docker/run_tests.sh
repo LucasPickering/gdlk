8a394bfd49ebed44bde6b479652435eafec9fd4c
Lucas Pickering <lucas@lucaspickering.me>
2020-04-30 16:36:47 -0400

    Set up cargo-make

--- api/docker/run_tests.sh
+++ api/docker/run_tests.sh
@@ -2,15 +2,5 @@
 
 set -ex
 
-dockerize -wait tcp://db:5432
-
-# Run seed data, just to make sure it's working
-diesel database reset
-for seed_file in ./seeds/*.sql; do
-    echo "Executing $seed_file..."
-    psql "$DATABASE_URL" < $seed_file
-done
-
-# Run tests
-diesel database reset
-cargo test -- --test-threads=1
+cargo make -p docker seed
+cargo make -p docker test
