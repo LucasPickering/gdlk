dae1acb0022e3464d2165741d9b86f12ac48f337
Lucas Pickering <lucas@lucaspickering.me>
2020-07-11 10:06:28 -0400

    Clean up some API auth code

--- api/src/models/user_provider.rs
+++ api/src/models/user_provider.rs
@@ -2,21 +2,8 @@ use crate::{
     models::{Factory, User},
     schema::user_providers,
 };
-use diesel::{
-    dsl,
-    expression::{bound::Bound, operators},
-    prelude::*,
-    query_builder::InsertStatement,
-    sql_types::Text,
-    Queryable,
-};
+use diesel::{prelude::*, query_builder::InsertStatement, Queryable};
 use uuid::Uuid;
-
-pub type WithSub<'a> =
-    dsl::Eq<user_providers::columns::sub, Bound<Text, &'a str>>;
-
-pub type WithProvider<'a> =
-    dsl::Eq<user_providers::columns::provider_name, Bound<Text, &'a str>>;
 
 #[derive(Clone, Debug, PartialEq, Identifiable, Associations, Queryable)]
 #[belongs_to(User, foreign_key = "user_id")]
@@ -26,20 +13,6 @@ pub struct UserProvider {
     pub sub: String,
     pub provider_name: String,
     pub user_id: Option<Uuid>,
-}
-
-impl UserProvider {
-    pub fn filter_by_sub_and_provider<'a>(
-        sub: &'a str,
-        provider: &'a str,
-    ) -> dsl::Filter<
-        user_providers::table,
-        operators::And<WithSub<'a>, WithProvider<'a>>,
-    > {
-        user_providers::table
-            .filter(user_providers::dsl::sub.eq(sub))
-            .filter(user_providers::dsl::provider_name.eq(provider))
-    }
 }
 
 #[derive(Debug, Default, PartialEq, Insertable)]
