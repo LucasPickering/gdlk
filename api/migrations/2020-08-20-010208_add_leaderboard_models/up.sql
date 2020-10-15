4458f2d8fbbc33f0a20c854709b6d97cfae0b815
Lucas Pickering <lucas@lucaspickering.me>
2020-10-15 18:46:28 -0400

    Remove user_program_pbs table

--- api/migrations/2020-08-20-010208_add_leaderboard_models/up.sql
+++ api/migrations/2020-08-20-010208_add_leaderboard_models/up.sql
@@ -1,28 +1,14 @@
 -- Metrics to measure the performance of a user_program
 CREATE TABLE user_program_records (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    program_spec_id UUID NOT NULL REFERENCES program_specs(id),
     source_code TEXT NOT NULL,
     cpu_cycles INTEGER NOT NULL CHECK(cpu_cycles >= 0),
     instructions INTEGER NOT NULL CHECK(instructions >= 0),
     registers_used INTEGER NOT NULL CHECK(registers_used >= 0),
     stacks_used INTEGER NOT NULL CHECK(stacks_used >= 0),
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
-
--- A user's best solution for a program in a particular stat
--- pb for "personal best"
-CREATE TABLE user_program_pbs (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID NOT NULL REFERENCES users(id),
-    program_spec_id UUID NOT NULL REFERENCES program_specs(id),
-    record_id UUID NOT NULL REFERENCES user_program_records(id),
-    stat TEXT NOT NULL CHECK(stat IN (
-        'cpu_cycles', 'instructions', 'registers_used', 'stacks_used'
-    )),
-    -- This col will duplicate the corresponding column on the record, but it's
-    -- more convenient having it here too
-    stat_value INTEGER NOT NULL CHECK (stat_value >= 0),
-    UNIQUE(user_id, program_spec_id, stat)
 );
 
 -- Each solution tracks stats for its last run. This column should be populated
