04e62938fa04a28d28f3462130eb00d7748ba92f
John Murray <5672686+JRMurr@users.noreply.github.com>
2020-11-11 14:24:06 -0500

    PR feedback

--- api/migrations/2020-11-08-174607_trigger_users/up.sql
+++ api/migrations/2020-11-08-174607_trigger_users/up.sql
@@ -1,35 +1,58 @@
-CREATE OR REPLACE FUNCTION delete_dangling_program_records()
-    RETURNS trigger AS
-$$
+CREATE INDEX user_program_records_user_program_ids ON user_program_records (program_spec_id, user_id);
+
+CREATE OR REPLACE FUNCTION delete_dangling_program_records ()
+    RETURNS TRIGGER
+    AS $$
 BEGIN
-    WITH pbs as (
-        SELECT min(cpu_cycles) as cpu_cycles, min(instructions) as instructions, min(registers_used) as registers_used, min(stacks_used) as stacks_used
-        FROM "user_program_records" as upr
-        WHERE upr.user_id = NEW.user_id 
-        AND upr.program_spec_id = NEW.program_spec_id
+    -- get the min for each stat so we know what this user's current personal bests are
+    WITH pbs AS (
+        SELECT
+            min(cpu_cycles) AS cpu_cycles,
+            min(instructions) AS instructions,
+            min(registers_used) AS registers_used,
+            min(stacks_used) AS stacks_used
+        FROM
+            "user_program_records"
+        WHERE
+            "user_program_records".user_id = NEW.user_id
+            AND "user_program_records".program_spec_id = NEW.program_spec_id
     )
-    DELETE FROM "user_program_records" WHERE id in (
-        SELECT id
-        FROM "user_program_records" as upr, pbs
-        WHERE upr.user_id = NEW.user_id AND upr.program_spec_id = NEW.program_spec_id
-        AND upr.cpu_cycles > pbs.cpu_cycles
-        AND upr.instructions > pbs.instructions
-        AND upr.registers_used > pbs.registers_used
-        AND upr.stacks_used > pbs.stacks_used
-        AND upr.id NOT IN (
+    DELETE FROM "user_program_records"
+    WHERE id IN (
+        SELECT
+            id
+        FROM
+            "user_program_records",
+            pbs
+        WHERE
+            -- Don't delete the record we just inserted since it won't be referenced yet
+            "user_program_records".id <> NEW.id
+            -- Only grab records by this user for this program spec
+            AND "user_program_records".user_id = NEW.user_id
+            AND "user_program_records".program_spec_id = NEW.program_spec_id
+            -- only delete records that are not a pb for any of the stats we track
+            AND "user_program_records".cpu_cycles > pbs.cpu_cycles
+            AND "user_program_records".instructions > pbs.instructions
+            AND "user_program_records".registers_used > pbs.registers_used
+            AND "user_program_records".stacks_used > pbs.stacks_used
             -- get all referenced rows so they wont be deleted
-            SELECT record_id FROM "user_programs" as up
-            WHERE up.user_id = NEW.user_id AND up.program_spec_id = NEW.program_spec_id
-        )
-        AND upr.id <> NEW.id
-    );
-RETURN NULL;
+            AND "user_program_records".id NOT IN (
+                SELECT
+                    record_id
+                FROM
+                    "user_programs"
+                WHERE
+                    "user_programs".user_id = NEW.user_id
+                    AND "user_programs".program_spec_id = NEW.program_spec_id
+            )
+        );
+    RETURN NULL;
 END;
 $$
 LANGUAGE 'plpgsql';
 
 CREATE TRIGGER delete_dangling_records
-    AFTER INSERT
-    ON "user_program_records"
+    AFTER INSERT ON "user_program_records"
     FOR EACH ROW
-    EXECUTE PROCEDURE delete_dangling_program_records();
+    EXECUTE PROCEDURE delete_dangling_program_records ();
+
