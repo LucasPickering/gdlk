92c47a24f2d9c62d0a24d13d9b9eaa0ce2512ac5
Lucas Pickering <lucas@lucaspickering.me>
2021-05-11 08:23:53 -0400

    Add copyPuzzleSolution mutation

--- api/core/fields.py
+++ api/core/fields.py
@@ -1,11 +1,12 @@
+import uuid
 from django.db import models
-from django.contrib.postgres.functions import RandomUUID
 
 
 class UUIDField(models.UUIDField):
     """
     A UUID field that serves as the primary key on a table. Minimal wrapper
-    around Django's UUIDField.
+    around Django's UUIDField. This field will be auto-populated during an
+    insert.
     """
 
     def __init__(self, *args, **kwargs):
@@ -13,6 +14,10 @@ class UUIDField(models.UUIDField):
             *args,
             primary_key=True,
             editable=False,
-            # Use the postgres function from pgcrypto for generating UUIDs
-            default=RandomUUID
+            # Generate a UUID **in Python** before insert. This is easier than
+            # generated the UUID in Postgres because:
+            # 1. We don't need an extra migration to enable the pg extension
+            # 2. Avoids django annoyances with loading the generated value
+            #   after insert
+            default=uuid.uuid4
         )
--- api/core/schema/mutations.py
+++ api/core/schema/mutations.py
@@ -1,13 +1,15 @@
+from django.db import IntegrityError
 import graphene
 from graphene_django.rest_framework.mutation import (
     ClientIDMutation,
     MutationOptions,
 )
+from graphql_relay import from_global_id
 from graphql_relay.connection.arrayconnection import offset_to_cursor
 from graphql import GraphQLError
 
 from .queries import PuzzleSolutionNode
-from ..models import Player
+from ..models import Player, PuzzleSolution
 from ..serializers import PuzzleSolutionSerializer
 from ..util import get_node_field_name
 
@@ -74,7 +76,15 @@ class NodeMutation(ClientIDMutation):
         super().__init_subclass_with_meta__(_meta=_meta, **options)
 
     @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
+    def mutate_and_get_row(cls, root, info, **input):
+        """
+        Execute the mutation and return the mutated row. By default this will
+        instantiate the mutation's serializer class and save the serializer.
+        The row should be of a model type that can map to this mutation's
+        returned node type. E.g. if this mutation returns PuzzleSolutionNode,
+        this method should return a PuzzleSolution.
+        """
+
         player = Player.get_or_create_for_user(info.context.user)
         serializer = cls._meta.serializer_class(
             data={
@@ -87,17 +97,24 @@ class NodeMutation(ClientIDMutation):
         )
 
         if serializer.is_valid():
-            node = serializer.save()
-            edge = cls._meta.edge_class(node=node, cursor=offset_to_cursor(0))
-            # Populate two output fields: one of just the node, one of the
-            # node wrapped in an edge.
-            return cls(
-                **{cls._meta.node_field: node, cls._meta.edge_field: edge}
-            )
+            return serializer.save()
         else:
             raise GraphQLError(
                 "Input validation error(s)", extensions=serializer.errors
             )
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        node = cls.mutate_and_get_row(root, info, **input)
+        edge = cls._meta.edge_class(node=node, cursor=offset_to_cursor(0))
+        # Populate two output fields: one of just the node, one of the
+        # node wrapped in an edge.
+        return cls(**{cls._meta.node_field: node, cls._meta.edge_field: edge})
+
+
+# TODO de-dupe some more of the logic between these. We should probably have
+# a simple way to do CUD operations for any node type (and potentially make it
+# easier to do extensions of that, like copy).
 
 
 class SavePuzzleSolutionMutation(NodeMutation):
@@ -105,6 +122,15 @@ class SavePuzzleSolutionMutation(NodeMutation):
     Create OR update a puzzle solution. Solutions are unique by their
     (name,player,puzzle) combo, so if a solution does not exist for that combo,
     a new one is created. If it does exist, the existing record is updated.
+
+    The create and update operations are combined here to make it easy for a
+    client to save a solution without having to know whether one exists for
+    that player+puzzle+name yet. Think of this as a file save, where it will
+    typically create the file if it doesn't existing before saving contents.
+
+    We may want to split this into two mutations if that ends up being the
+    pattern we use elsewhere, but for now it seems like it will be easier, at
+    least for a CLI client.
     """
 
     class Meta:
@@ -117,5 +143,73 @@ class SavePuzzleSolutionMutation(NodeMutation):
         source_code = graphene.String(required=True)
 
 
+class CopyPuzzleSolutionMutation(NodeMutation):
+    """
+    Copy a PuzzleSolution by ID. Returns the new puzzle solution. If provided,
+    this will use the given name as the name for the new copy. If not, a new
+    name will be generated.
+    """
+
+    class Meta:
+        node_class = PuzzleSolutionNode
+
+    class Input:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+
+    @classmethod
+    def mutate_and_get_row(cls, root, info, **input):
+        type, id = from_global_id(input["id"])
+        puzzle_solution = PuzzleSolution.objects.get(id=id)
+        puzzle_solution.id = None
+
+        # We need to pick a new name for the copy. If the user gave us a name
+        # to try, just use that. If it's already taken, we want to fail and
+        # report that to the user. If they _didn't_ give us a name, then find
+        # a new one. This option should _never fail_, so we may have to try
+        # multiple times.
+
+        if "name" in input:
+            puzzle_solution.name = input["name"]
+            # If this fails because of non-uniquness, we'll just raise an error
+            # and return to the user
+            puzzle_solution.save()
+        else:
+            # Keep incrementing the counter til we get a new name
+            base_name = puzzle_solution.name
+            counter = 1
+
+            while True:
+                puzzle_solution.name = f"{base_name} {counter}"
+                try:
+                    puzzle_solution.save()
+                    break
+                except IntegrityError:
+                    counter += 1
+
+        return puzzle_solution
+
+
+class DeletePuzzleSolutionMutation(NodeMutation):
+    """
+    Delete a PuzzleSolution by ID. Returns the deleted node (if it existed).
+    """
+
+    class Meta:
+        node_class = PuzzleSolutionNode
+
+    class Input:
+        id = graphene.ID(required=True)
+
+    @classmethod
+    def mutate_and_get_row(cls, root, info, **input):
+        type, id = from_global_id(input["id"])
+        puzzle_solution = PuzzleSolution.objects.get(id=id)
+        puzzle_solution.delete()
+        return puzzle_solution
+
+
 class Mutation(graphene.ObjectType):
     save_puzzle_solution = SavePuzzleSolutionMutation.Field()
+    copy_puzzle_solution = CopyPuzzleSolutionMutation.Field()
+    delete_puzzle_solution = DeletePuzzleSolutionMutation.Field()
