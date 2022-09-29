63b2ad364dda2a46aacadf922d5338eff3b7badd
Lucas Pickering <lucas@lucaspickering.me>
2022-09-29 08:10:42 -0400

    Initial (new) web API

---
+++ api/m.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run a manage.py command. If not in the api container already, this will exec into it
+
+if test -f "/.dockerenv" || [ -n "$KUBERNETES_SERVICE_HOST" ]; then
+    cd src && poetry run ./manage.py "$@"
+else
+    # Not in docker yet, re-run the command within the container
+    docker exec -it $(docker-compose ps -q api) ./m.sh "$@"
+fi
